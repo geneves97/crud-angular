a79cd75fb2fc796015ea0aa3cc32f07cbca9cf1e
george <georgeneves.dev@gmail.com>
2023-10-14 01:12:32 -0300

    donfigurnado o onError no course component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, catchError, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
 import { CoursesService } from '../service/courses.service';
-import { Observable, catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -13,16 +15,26 @@ export class CoursesComponent implements OnInit{
   courses$: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor(private coursesService: CoursesService){
+  constructor(
+    public dialog: MatDialog,
+    private coursesService: CoursesService){
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
-        console.log(error)
+        this.onError('Erro ao carregar cursos')
         return of([]);
       })
       );
   }
   
+  onError(errorMsg: string){
+    this.dialog.open(ErrorDialogComponent, {
+      data: {
+        errorMsg
+      },
+    });
+  }
+
   ngOnInit(): void {
   }
 
