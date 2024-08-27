c6fa798419ea278b475f016c9907ee1995e8a8c7
George Neves <georgeneves.dev@gmail.com>
2024-08-27 00:41:22 -0300

    refactor: Angular typds Forms

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../service/courses.service';
 
@@ -11,26 +11,26 @@ import { CoursesService } from '../service/courses.service';
 })
 export class CourseFormComponent implements OnInit{
   
-  form: FormGroup; 
-
-  constructor(private formBuilder: FormBuilder,
+  form= this.formBuilder.group({
+    name:[''],
+    category:['']
+  });
+  constructor(private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location
   ){
-    this.form = this.formBuilder.group({
-      name:[null],
-      category:[null]
-    });
+    //this.form 
   }
 
   ngOnInit(): void {
  }
 
  onSubmit() {
-  this.service.save(this.form.value).subscribe({  
-             next: (result: any) => this.OnSuccess(),
-             error: () => this.onError()
+  this.service.save(this.form.value)
+            .subscribe({ 
+              next: (result: any) => this.OnSuccess(),
+              error: () => this.onError()
   });
 }
 
