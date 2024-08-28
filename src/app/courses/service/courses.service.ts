be8ff6842c1aacd7418a5c30d94b737909dbee9c
George Neves <georgeneves.dev@gmail.com>
2024-08-28 09:50:36 -0300

    feat:  updating the save method with update and create

--- src/app/courses/service/courses.service.ts
+++ src/app/courses/service/courses.service.ts
@@ -26,6 +26,17 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>){
+    if(record.id){
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>){
     return this.httClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return  this.httClient.put<Course>(`${this.API}/${record.id}`, record).pipe(first());
   }
 }
